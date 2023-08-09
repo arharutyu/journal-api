ce0cb41000a8f59261e53bfda4a71b4ac3c3c858
arharuty <13937@coderacademy.edu.au>
2023-08-10 09:13:48 +1000

    add cors

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express'
 import { EntryModel, CategoryModel } from './db.js'
 import entryRoutes from './routes/entry_routes.js'
 import catRoutes from './routes/category_routes.js'
+import cors from 'cors'
 
 // initialize express instance
 const app = express()
 const port = 4001
+
+app.use(cors())
 
 // middleware to accept JSON data in request body
 app.use(express.json())
