9b0ff88a9db71c9276ca836fc825bb64374994d8
arharuty <13937@coderacademy.edu.au>
2023-08-13 09:36:08 +1000

    refactor index to new app file

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import { EntryModel, CategoryModel } from './db.js'
+import entryRoutes from './routes/entry_routes.js'
+import catRoutes from './routes/category_routes.js'
+import cors from 'cors'
+
+// initialize express instance
+const app = express()
+
+
+app.use(cors())
+
+// middleware to accept JSON data in request body
+app.use(express.json())
+
+// GET route for home page
+app.get('/', (request, response) => response.send({ info: 'Journal API!'}))
+
+
+app.use('/entries', entryRoutes)
+
+app.use('/categories', catRoutes)
+
+
+
+export default app
--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-import express from 'express'
-import { EntryModel, CategoryModel } from './db.js'
-import entryRoutes from './routes/entry_routes.js'
-import catRoutes from './routes/category_routes.js'
-import cors from 'cors'
+import app from './app.js'
 
-// initialize express instance
-const app = express()
 const port = 4001
-
-app.use(cors())
-
-// middleware to accept JSON data in request body
-app.use(express.json())
-
-// GET route for home page
-app.get('/', (request, response) => response.send({ info: 'Journal API!'}))
-
-
-app.use('/entries', entryRoutes)
-
-app.use('/categories', catRoutes)
 
 app.listen(port)
