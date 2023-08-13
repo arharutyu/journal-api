ecef6578d8f4c580a91cd51c660257a855c60e18
arharuty <13937@coderacademy.edu.au>
2023-08-13 11:50:07 +1000

    refactor entry routes tests

--- app.test.js
+++ app.test.js
@@ -1,6 +1,8 @@
 import app from './app.js'
 import request from 'supertest'
 
+const validNames = ['Food', 'Gaming', 'Coding', 'Other']
+
 describe("App Test", () => {
   test('GET /', async () => {
     const res = await request(app).get('/')
@@ -8,28 +10,5 @@ describe("App Test", () => {
     expect(res.header['content-type']).toMatch('application/json')
     expect(res.body.info).toBeDefined()
     expect(res.body.info).toBe('Journal API!')
-  })
-
-describe('GET /categories', () => {
-  let res
-
-  beforeEach(async () => {
-    res = await request(app).get('/categories')
-  })
-  test('Returns JSON', () => {
-    expect(res.status).toBe(200)
-    expect(res.header['content-type']).toMatch('application/json') 
-  })
-
-  test('Returns an array of 4 elements', () => {
-    expect(res.body).toBeInstanceOf(Array)
-    expect(res.body).toHaveLength(4)
-  })
-
-  test('First category has a key "name" with value "Food"', () => {
-    expect(res.body[0]).toBeDefined()
-    expect(res.body[0].name).toBe('Food')
-    
-    })
   })
 })
---
+++ routes/entry_routes.test.js
@@ -0,0 +1,58 @@
+import app from '../app.js'
+import request from 'supertest'
+
+const validNames = ['Food', 'Gaming', 'Coding', 'Other']
+
+describe('POST /entries', () => {
+    let res
+
+    beforeAll(async () => {
+      res = await request(app).post('/entries').send({
+        category: "Food",
+        content: "Blegh"
+      })
+    })
+    
+    test('Return a JSON body with _id', () => {
+      expect(res.status).toBe(201)
+      expect(res.header['content-type']).toMatch('json')
+      expect(res.body._id).toBeDefined()
+    })
+
+    test('Category has _id and correct name', () => {
+      expect(res.body.category).toBeDefined()
+      expect(res.body.category._id).toBeDefined()
+      expect(res.body.category.name).toBeDefined()
+      expect(res.body.category.name).toBe('Food')
+    })
+
+    test('Content has the correct value', () => {
+      expect(res.body.content).toBeDefined()
+      expect(res.body.content).toBe('Blegh')
+    })
+  })
+
+describe('GET /categories', () => {
+  let res
+
+  beforeEach(async () => {
+    res = await request(app).get('/categories')
+  })
+  test('Returns JSON', () => {
+    expect(res.status).toBe(200)
+    expect(res.header['content-type']).toMatch('application/json') 
+  })
+
+  test('Returns an array of 4 elements', () => {
+    expect(res.body).toBeInstanceOf(Array)
+    expect(res.body).toHaveLength(4)
+  })
+
+  test('Each category has a valid "name" and "_id"', () => {
+    res.body.forEach(el => {
+      expect(el.name).toBeDefined()
+      expect(el._id).toBeDefined()
+      expect(validNames).toContain(el.name)
+      })
+    })
+  })
